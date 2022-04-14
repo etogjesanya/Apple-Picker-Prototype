41fb8b9010f8fbff7a168fda6e2301e638f4339e
etogjesanya <Alexander.Al-M@yandex.ru>
2022-04-14 21:26:23 +0300

    21:26 14.04.2022

--- ZOV/Assets/Scripts/CubeSpawner.cs
+++ ZOV/Assets/Scripts/CubeSpawner.cs
@@ -5,16 +5,43 @@ using UnityEngine;
 public class CubeSpawner : MonoBehaviour
 {
     public GameObject cubePrefabVar;
+    public List<GameObject> GameObjectList;
+    public float scalingFactor = 0.95f;
+    public int numCubes = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //   Instantiate(cubePrefabVar);
+        GameObjectList = new List<GameObject>();
     }
 
         // Update is called once per frame
         void Update()
         {
-            Instantiate(cubePrefabVar);
+            numCubes++;
+            GameObject gObj = Instantiate<GameObject>(cubePrefabVar);
+
+            gObj.name = "Cube " + numCubes;
+            Color c = new Color(Random.value, Random.value, Random.value);
+            gObj.GetComponent<Renderer>().material.color = c;
+            gObj.transform.position = Random.insideUnitSphere;
+
+            GameObjectList.Add(gObj);
+            List<GameObject> removeList = new List<GameObject>();
+
+            foreach(GameObject goTemp in GameObjectList){
+                float scale = goTemp.transform.localScale.x;
+                scale *= scalingFactor;
+                goTemp.transform.localScale = Vector3.one * scale;
+
+                if (scale <= 0.01f){
+                    removeList.Add(goTemp);
+                }
+            }
+
+            foreach(GameObject goTemp in removeList){
+                GameObjectList.Remove(goTemp);
+                Destroy(goTemp);
+            }
+            
         }
     }
